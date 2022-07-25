b8dc3f7c28645dc54c86b627b74f790541f5874f
Lachlan Kalf <lachlankalf@gmail.com>
2022-07-25 19:52:33 +1000

    Internal Name Fix

--- CameraTest/Properties/AssemblyInfo.cs
+++ CameraTest/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Capture Display")]
-[assembly: AssemblyDescription("Disaply the Capture From Shitty USB Capture cards")]
+[assembly: AssemblyDescription("Display the Capture From Shitty USB Capture cards")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Glitcher INC")]
 [assembly: AssemblyProduct("Capture Display")]
