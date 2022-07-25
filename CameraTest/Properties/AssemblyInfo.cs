33d4153cfc604f1c511c1a1f6b01ad23da84db11
Lachlan Kalf <lachlankalf@gmail.com>
2022-07-25 16:45:40 +1000

    Fixes

--- CameraTest/Properties/AssemblyInfo.cs
+++ CameraTest/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CameraTest")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Capture Display")]
+[assembly: AssemblyDescription("Disaply the Capture From Shitty USB Capture cards")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CameraTest")]
+[assembly: AssemblyCompany("Glitcher INC")]
+[assembly: AssemblyProduct("Capture Display")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
